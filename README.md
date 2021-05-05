3241f6bf57bf979f7c7490a68b5757da1cda3d9d
tanpu16 <priyankatanpure1995@gmail.com>
2021-05-05 11:49:04 +0530

    adding code,readme, result files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Naive-Bayes-for-text-classification-spam-ham
+
+Download the spam/ham (ham is not spam) dataset available on my-
+Courses. The data set is divided into two sets: training set and test set.
+The dataset was used in the Metsis et al. paper [1]. Each set has two di-
+rectories: spam and ham. All files in the spam folders are spam messages
+and all files in the ham folder are legitimate (non spam) messages.
+
+Implement the multinomial Naive Bayes algorithm for text classification
+described here: http://nlp.stanford.edu/IR-book/pdf/13bayes.pdf
+(see Figure 13.2). Note that the algorithm uses add-one laplace smooth-
+ing. Ignore punctuation and special characters and normalize words by
+converting them to lower case, converting plural words to singular (i.e.,
+\Here" and \here" are the same word, \pens" and \pen" are the same
+word). Normalize words by stemming them using an online stemmer such
+as http://www.nltk.org/howto/stem.html. Make sure that you do all
+the calculations in log-scale to avoid under
+ow. Use your algorithm to
+learn from the training set and report accuracy on the test set.
+
+Improve your Naive Bayes by throwing away (i.e., filtering out) stop words
+such as \the" \of" and \for" from all the documents. A list of stop words
+can be found here: http://www.ranks.nl/stopwords. Report accuracy
+for Naive Bayes for this filtered set. Does the accuracy improve? Explain
+why the accuracy improves or why it does not?
---
+++ code.py
@@ -0,0 +1,233 @@
+'''
+Declaration :
+I have done this assignment completely on my own. I have not copied it, nor have I given my solution to anyone else. I understand that if I am involved in plagiarism or cheating I will have to sign an official form that I have cheated and that this form will be stored in my official university record. I also understand that I will receive a grade of 0 for the involved assignment for my first offense and that I will receive a grade of F for the course for any additional offense.
+-Priyanka Prakash Tanpure
+'''
+
+import os
+import pandas as pd
+import re
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords 
+import math
+ 
+nltk.download('stopwords')
+
+path_spam = "train/spam/"
+path_ham = "train/ham/"
+
+stemmer = SnowballStemmer("english")
+stop_words = set(stopwords.words('english'))
+
+totalspamwordcount = 0
+totalspamfilecount = 0
+totalhamwordcount = 0
+totalhamfilecount = 0
+spam_words_prob = {} #spam word and it's conditional probability
+ham_words_prob = {}   #ham word and it's conditional probability
+spam_words_dict ={}    #word and it's count in spam train files
+ham_words_dict={}     ##word and it's count in ham train files
+prior_spam = 0
+prior_ham =0
+spam_no_word_probability = 0
+ham_no_word_probability = 0
+
+totalCorrectPredSpam = 0;
+totalCorrectPredHam = 0;
+
+
+def processfiles(file_path,is_stop_param):
+    f= open(file_path,'r',errors='ignore')
+    #text = f.read().lower()
+    wordlist = list()
+    for line in f:
+        line = line.rstrip()
+        words = re.sub('[^a-zA-Z0-9 ]','',line).split()
+        for word in words:
+            wordlist.append(word)
+    wordlist = [stemmer.stem(word) for word in list(filter(None,wordlist))]
+    if is_stop_param == True:
+        wordlist = [word for word in wordlist if not word in stop_words]
+    return wordlist
+
+
+def multiNomialNBModel(is_stop_param):
+    
+    global totalspamwordcount,totalspamfilecount,totalhamwordcount,totalhamfilecount,spam_words_dict,ham_words_dict
+    global prior_spam,prior_ham,spam_words_prob,ham_words_prob,spam_no_word_probability,ham_no_word_probability
+            
+    for file in os.listdir(path_spam):
+        if file.endswith(".txt"):
+            file_path = f"{path_spam}{file}"
+            wordlistspam = processfiles(file_path,is_stop_param)
+            for word in wordlistspam:
+                if word in spam_words_dict:
+                    spam_words_dict[word] +=1
+                else:
+                    spam_words_dict[word] =1
+                    spam_words_prob[word] = 0
+                totalspamwordcount +=1
+            totalspamfilecount+=1
+
+
+    for file in os.listdir(path_ham):
+        if file.endswith(".txt"):
+            file_path = f"{path_ham}{file}"
+            wordlistham = processfiles(file_path,is_stop_param)
+            for word in wordlistham:
+                if word in ham_words_dict:
+                    ham_words_dict[word] +=1
+                else:
+                    ham_words_dict[word] =1
+                    ham_words_prob[word] = 0
+                totalhamwordcount +=1
+            totalhamfilecount+=1
+    
+    prior_spam = totalspamfilecount/(totalspamfilecount+totalhamfilecount)
+    prior_ham = totalhamfilecount/(totalspamfilecount+totalhamfilecount)
+    prior_spam = math.log(prior_spam)
+    prior_ham = math.log(prior_ham)
+    
+    
+    for word in spam_words_prob:
+        k=1
+        count_word = spam_words_dict[word]
+        spam_words_prob[word] = math.log((count_word+k)/(totalspamwordcount+(k*len(spam_words_dict))))
+        
+    for word in ham_words_prob:
+        k=1
+        count_word = ham_words_dict[word]
+        ham_words_prob[word] = math.log((count_word+k)/(totalhamwordcount+(k*len(ham_words_dict))))
+    #for all the words not in spam or ham vocab
+    k=1
+    spam_no_word_probability = math.log(k/(totalspamwordcount+(k*len(spam_words_dict))))
+    ham_no_word_probability = math.log((k/(totalhamwordcount+(k*len(ham_words_dict)))))
+
+def applyMultinomialNB(is_stop_param, is_train):
+
+    global spam_words_prob,ham_words_prob,spam_no_word_probability,ham_no_word_probability
+    path_spam_test = "test/spam/"
+    path_ham_test = "test/ham/"
+    
+    if is_train == True:
+        path_spam_test = "train/spam/"
+        path_ham_test = "train/ham/"
+    
+    accurate_prediction = 0
+    total_word_count = 0
+    
+    for file in os.listdir(path_spam_test):
+        if file.endswith(".txt"):
+            file_path = f"{path_spam_test}{file}"
+            wordlistspamtest = processfiles(file_path,is_stop_param)
+            spam_words_prob_test = {}
+            ham_words_prob_test = {}
+            spam_words_dict_test = {}
+            ham_words_dict_test = {}
+            for word in wordlistspamtest:
+                k=1  #laplace smoothing
+                #for calculating probability of word given spam
+                if word not in spam_words_dict_test :
+                    spam_words_dict_test[word] =1
+                    if word in spam_words_dict:
+                        spam_words_prob_test[word] = spam_words_prob[word]
+                    else:
+                        spam_words_prob_test[word] = spam_no_word_probability
+                else:
+                        spam_words_dict_test[word] +=1
+                        
+                #for calculating probability of word given spam
+                if word not in ham_words_dict_test :
+                    ham_words_dict_test[word] =1
+                    if word in ham_words_dict:
+                        ham_words_prob_test[word] = ham_words_prob[word]
+                    else:
+                        ham_words_prob_test[word] = ham_no_word_probability
+                else:
+                        ham_words_dict_test[word] +=1
+
+            #spam probaability
+            spam_probability = prior_spam
+            for w in spam_words_prob_test:
+                spam_probability = spam_probability + (spam_words_prob_test[w]*spam_words_dict_test[w])
+            spam_probability = math.exp(spam_probability)
+            #ham probability
+            ham_probability = prior_ham
+            for w in ham_words_prob_test:
+                ham_probability = ham_probability + (ham_words_prob_test[w]*ham_words_dict_test[w])
+            ham_probability = math.exp(ham_probability)
+            
+            if spam_probability >= ham_probability:
+                accurate_prediction +=1
+            total_word_count +=1
+            
+        for file in os.listdir(path_ham_test):
+            if file.endswith(".txt"):
+                file_path = f"{path_ham_test}{file}"
+                wordlisthamtest = processfiles(file_path,is_stop_param)
+                spam_words_prob_test = {}
+                ham_words_prob_test = {}
+                spam_words_dict_test = {}
+                ham_words_dict_test = {}
+                for word in wordlisthamtest:
+                    k=1  #laplace smoothing
+                    #for calculating probability of word given spam
+                    if word not in spam_words_dict_test :
+                        spam_words_dict_test[word] =1
+                        if word in spam_words_dict:
+                            spam_words_prob_test[word] = spam_words_prob[word]
+                        else:
+                            spam_words_prob_test[word] = spam_no_word_probability
+                    else:
+                            spam_words_dict_test[word] +=1
+
+                    #for calculating probability of word given spam
+                    if word not in ham_words_dict_test :
+                        ham_words_dict_test[word] =1
+                        if word in ham_words_dict:
+                            ham_words_prob_test[word] = ham_words_prob[word]
+                        else:
+                            ham_words_prob_test[word] = ham_no_word_probability
+                    else:
+                            ham_words_dict_test[word] +=1
+
+                #spam probaability
+                spam_probability = prior_spam
+                for w in spam_words_prob_test:
+                    spam_probability = spam_probability + (spam_words_prob_test[w]*spam_words_dict_test[w])
+                spam_probability = math.exp(spam_probability)
+                #ham probability
+                ham_probability = prior_ham
+                for w in ham_words_prob_test:
+                    ham_probability = ham_probability + (ham_words_prob_test[w]*ham_words_dict_test[w])
+                ham_probability = math.exp(ham_probability)
+
+                if ham_probability >= spam_probability:
+                    accurate_prediction +=1
+                total_word_count +=1
+
+        accuracy = accurate_prediction/total_word_count
+        return accuracy
+		
+print("******************Train Set***************")
+print("With stop word :")
+multiNomialNBModel(False)
+accuracy_ws = applyMultinomialNB(False,True)
+print("Accuracy on train set with stop word : {}".format(accuracy_ws))
+
+# print("\nWithout stop word :")
+# multiNomialNBModel(True)
+# accuracy_s = applyMultinomialNB(True,True)
+# print("Accuracy on train set without stop word : {}".format(accuracy_s))
+
+print("\n\n******************Test Set***************")
+print("With stop word :")
+multiNomialNBModel(False)
+accuracy_ws = applyMultinomialNB(False,False)
+print("Accuracy on test set with stop word : {}".format(accuracy_ws))
+
+print("\nWithout stop word :")
+multiNomialNBModel(True)
+accuracy_s = applyMultinomialNB(True,False)
+print("Accuracy on test set without stop word : {}".format(accuracy_s))
